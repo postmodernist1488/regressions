432a5b244d773ce4b898818ab1760648352063fb
postmodernist1488 <elitic.pantheism@gmail.com>
2023-07-12 14:18:52 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <raylib.h>
+
+const int screen_width = 1000;
+const int screen_height = 800;
+
+int main() {
+
+    InitWindow(screen_width, screen_height, "Regressions");
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+
+    
+        EndDrawing();
+    }
+
+    return 0;
+}
