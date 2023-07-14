6e21770fcc28c49dc83350bd9a278ebdeef96ddb
postmodernist1488 <elitic.pantheism@gmail.com>
2023-07-15 01:40:33 +0300

    add diffs for linear and quadratic and formulas for power and exponential regressions

--- solutions.py
+++ solutions.py
@@ -1,13 +1,20 @@
-from sympy import solve, diff
-from sympy import Symbol, symbols, Eq, Pow
+from sympy import simplify, solve, diff
+from sympy import Symbol, Eq, Pow, ln, exp
+from sympy.abc import a, b, c, x, y
 
-def linear():
+def linear(show_diff=True):
+    print('Linear regression:')
     sx2 = Symbol('sx2')
     sx = Symbol('sx')
     sxy = Symbol('sxy')
     sy = Symbol('sy')
     n   = Symbol('n')
-    a, b = symbols('a,b')
+
+    if show_diff:
+        error = (a * x + b - y)**2
+        diff_a, diff_b = diff(error, a), diff(error, b)
+        print('d(ax + b)/da =', diff_a)
+        print('d(ax + b)/db =', diff_b)
 
     equations = [
         a*sx2 + b*sx - sxy,
@@ -17,8 +24,12 @@ def linear():
     # упрощение
     simplified = solve([equations[1], Eq(a, sol[a])], [b])
     sol[b] = simplified[b]
-    return sol
-def quadratic():
+
+    for key, value in sol.items():
+        print(f'{key} = {value}')
+
+def quadratic(show_diff=True):
+    print('Quadratic regression:')
     sx4 = Symbol('sx4') 
     sx3 = Symbol('sx3')
     sx2 = Symbol('sx2')
@@ -27,7 +38,13 @@ def quadratic():
     sxy = Symbol('sxy')
     sy = Symbol('sy')
     n   = Symbol('n')
-    a, b, c = symbols('a,b,c')
+
+    if show_diff:
+        error = (a * x**2 + b * x + c - y)**2
+        diff_a, diff_b, diff_c = diff(error, a), diff(error, b), diff(error, c)
+        print('d(ax^2 + bx + c)/da =', diff_a)
+        print('d(ax^2 + bx + c)/db =', diff_b)
+        print('d(ax^2 + bx + c)/dc =', diff_b)
 
     equations = [
         a*sx4 + b*sx3 + c*sx2 - sx2y,
@@ -41,29 +58,71 @@ def quadratic():
     # подставим b в 3 уравнение
     simplified = solve([equations[2], Eq(b, sol[b])], [c])
     sol[c] = simplified[c]
-    return sol
 
-def power():
-    a, b, x, y = symbols('a,b,x,y')
-    error = (a*Pow(x, b) - y)**2
-    return diff(error, a), diff(error, b)
-
-
-if __name__ == '__main__':
-    print('Linear regression:')
-    linear_sol = linear()
-    for key, value in linear_sol.items():
+    for key, value in sol.items():
         print(f'{key} = {value}')
-    print()
 
-    print('Quadratic regression:')
-    quadratic_sol = quadratic()
-    for key, value in quadratic_sol.items():
-        print(f'{key} = {value}')
-    print()
-
+def power(show_diff=True):
     print('Power regression:')
-    diff_a, diff_b = power()
-    print('d ax^b/da =', diff_a)
-    print('d ax^b/db =', diff_b)
+    sln2x = Symbol('sln2x')
+    slnx = Symbol('slnx')
+    slnxlny = Symbol('slnxlny')
+    slny = Symbol('slny')
+    n   = Symbol('n')
+    
+    lna = Symbol('lna')
+
+    if show_diff:
+        error = (a*Pow(x, b) - y)**2
+        diff_a, diff_b = diff(error, a), diff(error, b)
+        print('d(ax^b)/da =', diff_a)
+        print('d(ax^b)/db =', diff_b)
+
+    equations = [
+        b*sln2x + lna*slnx - slnxlny,
+        b*slnx + lna*n - slny
+    ]
+    sol = solve(equations, [lna, b], dict=True)[0]
+    simplified = solve([equations[1], Eq(b, sol[b])], [lna], dict=True)[0]
+    sol[lna] = simplified[lna]
+    print(f'b = {sol[b]}')
+    print(f'a = {exp(simplified[lna])}')
+
+def exponential(show_diff=True):
+    print('Exponential regression:')
+    sx2 = Symbol('sx2')
+    sx = Symbol('sx')
+    sxlny = Symbol('sxlny')
+    slny = Symbol('slny')
+    n   = Symbol('n')
+
+    lna = Symbol('lna')
+    lnb = Symbol('lnb')
+
+    if show_diff:
+        error = (a*Pow(b, x) - y)**2
+        diff_a, diff_b = diff(error, a), diff(error, b)
+        print('d(ab^x)/da =', diff_a)
+        print('d(ab^x)/db =', diff_b)
+
+    equations = [
+        lnb*sx2 + lna*sx - sxlny,
+        lnb*sx + lna*n - slny
+    ]
+    sol = solve(equations, [lna, lnb], dict=True)[0]
+    simplified = solve([equations[1], Eq(lnb, sol[lnb])], [lna], dict=True)[0]
+    print(f'b = {exp(sol[lnb])}')
+    print(f'a = {exp(simplified[lna])}')
+
+#TODO: diff + formula
+if __name__ == '__main__':
+    show_diff = True
+    linear(show_diff)
+    print()
+    quadratic(show_diff)
+    print()
+    power(show_diff)
+    print()
+    exponential(show_diff)
+
 
